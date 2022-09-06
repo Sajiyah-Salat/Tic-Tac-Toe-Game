d0b887c4ecd43fa3d80c551d945d57edf7434501
unknown <salatsajiya7.8.6@gmail.com>
2022-09-06 19:11:41 +0530

    work is remaining

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+const changeTurn = ()=>{
+    return turn === "X"?"0":"X"
+}
+const checkWin = ()=>{
+
+}
+// Game Logic
+// This code is remaining as I am learning js 
